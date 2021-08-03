ec63d4752db8527b545495afba6d41bbf21f4d59
Axel Galicia <axelgalicia@gmail.com>
2021-08-03 01:14:15 -0400

    add: Adding Semantic UI as a UI Library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import { Container, Header, Divider, Segment, Icon, List } from 'semantic-ui-react';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Container textAlign="center">
+          <Icon
+            float="center"
+            color="blue"
+            name="search"
+            size="massive"
+          ></Icon>
+        </Container>
+
+        <Header
+          color="blue"
+          textAlign="center"
+          as="h1"
+          content="Ledn Users Search"
+          subheader="Token holder's accounts"
+        />
+
+        <Container textAlign="left">
+          <List>
+            <List.Item>
+              <List.Icon name='users' />
+              <List.Content>Axel Galicia</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='marker' />
+              <List.Content>Toronto, ON. Canada</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='mail' />
+              <List.Content>
+                <a href='mailto:jack@semantic-ui.com'>axelgalicia@gmail.com</a>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name='linkify' />
+              <List.Content>
+                <a href='https://axel.today.com'>axel.today</a>
+              </List.Content>
+            </List.Item>
+          </List>
+        </Container>
+
+        <Divider />
+
+        <Segment>
+          <Header as='h3'>User's Data</Header>
+          <Divider section />
+
+
+        </Segment>
+
+        <Segment>
+          <Header as='h3'>Searching fields</Header>
+          <Divider section />
+
+          <Header as='h3'>Filtering</Header>
+          <Divider section />
+
+          <Header as='h3'>Sorting</Header>
+          <Divider section />
+        </Segment>
+      </Container>
+
+
     </div>
   );
 }
+
+
+
 
 export default App;
